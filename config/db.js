c44f04874af0c48149a26beb886926e04028a1e3
Sumit Saini <sumitsaini5877@gmail.com>
2024-11-17 21:12:01 +0530

    cloud db setup

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose')
 
 const connectDB = async () => {
   try {
+    
     await mongoose.connect(process.env.MONGO_URL);
     console.log('Connected to MongoDB successfully!');
   } catch (error) {
